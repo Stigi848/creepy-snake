8e1984af8b91e7d49cdc78cfb1410cc224330760
Stigi848 <60348381+Stigi848@users.noreply.github.com>
2020-03-27 17:57:09 +0100

    1.0

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Game from './Game';
 import './style.css';
+// 27.03 Koniec kursu Coders Lab -projekt zaliczeniowy 
 
 
 ReactDOM.render(
   <Game />,
   document.getElementById('root'));
-
-//
